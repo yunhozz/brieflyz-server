b7711541eed7dcbfe0b8175cef8f347fdb5fcf57
Yunho Park <qkrdbsgh0111@gmail.com>
2025-06-05 16:52:50 +0900

    [#1] STYLE: open 키워드 제거

--- auth-service/src/main/kotlin/io/brieflyz/auth_service/AuthServiceApplication.kt
+++ auth-service/src/main/kotlin/io/brieflyz/auth_service/AuthServiceApplication.kt
@@ -4,7 +4,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-open class AuthServiceApplication
+class AuthServiceApplication
 
 fun main(args: Array<String>) {
 	runApplication<AuthServiceApplication>(*args)
