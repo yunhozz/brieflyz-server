c337f71efff549bb6f03e20b6c6aa851773f9127
Yunho Park <qkrdbsgh0111@gmail.com>
2025-06-12 20:19:35 +0900

    [#2] Initial Commit

---
+++ user-service/src/main/kotlin/io/brieflyz/user_service/config/AuditingConfig.kt
@@ -0,0 +1,8 @@
+package io.brieflyz.user_service.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.r2dbc.config.EnableR2dbcAuditing
+
+@Configuration
+@EnableR2dbcAuditing
+class AuditingConfig
---
+++ user-service/src/main/kotlin/io/brieflyz/user_service/config/KafkaConfig.kt
@@ -0,0 +1,8 @@
+package io.brieflyz.user_service.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.kafka.annotation.EnableKafka
+
+@Configuration
+@EnableKafka
+class KafkaConfig {}
