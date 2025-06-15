3ced852eeeba51f49f8552cc6b15229232b7820b
Yunho Park <qkrdbsgh0111@gmail.com>
2025-06-15 20:42:11 +0900

    [#1] CHORE: kafka 설정 추가

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -1,5 +1,6 @@
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.springframework.boot:spring-boot-starter-kafka")
     implementation("org.springframework.boot:spring-boot-starter-log4j2")
     implementation("org.springframework.kafka:spring-kafka")
     testImplementation("org.springframework.kafka:spring-kafka-test")
