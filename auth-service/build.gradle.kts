f71d7824c829992da4c5a75a55562f31b91cc896
Yunho Park <qkrdbsgh0111@gmail.com>
2025-06-26 19:46:33 +0900

    [#1] CHORE: Redisson 의존성 추가

--- auth-service/build.gradle.kts
+++ auth-service/build.gradle.kts
@@ -1,5 +1,6 @@
 dependencies {
     implementation(project(":core"))
+    // Spring Boot
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-data-redis")
@@ -7,17 +8,25 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-web")
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
+    // Spring Cloud
+    implementation("org.springframework.cloud:spring-cloud-starter-config")
+    // Kafka
+    implementation("org.springframework.kafka:spring-kafka")
+    // Module
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
-    implementation("org.springframework.cloud:spring-cloud-starter-config")
-    implementation("org.springframework.kafka:spring-kafka")
     // JWT
     implementation("io.jsonwebtoken:jjwt-api:0.11.5")
     runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.5")
     runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.5")
-
-    developmentOnly("org.springframework.boot:spring-boot-devtools")
+    // Redisson
+    implementation("org.redisson:redisson-spring-boot-starter:3.50.0")
+    // khttp
+    implementation("org.danilopianini:khttp:1.6.3")
+    // MySQL
     runtimeOnly("com.mysql:mysql-connector-j")
+    // Test
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testImplementation("org.springframework.kafka:spring-kafka-test")
