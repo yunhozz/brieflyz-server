f14449bc490cd67b37a1f6f3753cf762af1c4ba0
Yunho Park <qkrdbsgh0111@gmail.com>
2025-06-26 17:36:11 +0900

    [#1] REFACTOR: 객체 생성 시 공개 범위 제한

--- core/src/main/kotlin/io/brieflyz/core/dto/api/ApiResponse.kt
+++ core/src/main/kotlin/io/brieflyz/core/dto/api/ApiResponse.kt
@@ -5,46 +5,60 @@ import org.springframework.validation.BindingResult
 import java.time.LocalDateTime
 
 data class ApiResponse<T> private constructor(
-    val header: ApiHeader,
-    val body: ApiBody<T>
+    private val header: ApiHeader,
+    private val body: ApiBody<T>
 ) {
     companion object {
-        fun <T> success(successCode: SuccessCode, data: T? = null): ApiResponse<T> {
-            val header = ApiHeader(successCode, true)
-            val body = ApiBody(successCode.message, data)
+        fun <T : Any> success(successCode: SuccessCode, data: T? = null): ApiResponse<T> {
+            val header = ApiHeader.of(successCode, true)
+            val body = ApiBody.of(successCode.message, data)
             return ApiResponse(header, body)
         }
 
         fun fail(errorCode: ErrorCode, errorData: ErrorData): ApiResponse<ErrorData> {
-            val header = ApiHeader(errorCode, false)
-            val body = ApiBody(errorCode.message, errorData)
+            val header = ApiHeader.of(errorCode, false)
+            val body = ApiBody.of(errorCode.message, errorData)
             return ApiResponse(header, body)
         }
     }
 
-    data class ApiHeader(val code: ApiResponseCode, val success: Boolean)
+    data class ApiHeader private constructor(
+        private val code: ApiResponseCode,
+        private val success: Boolean
+    ) {
+        companion object {
+            internal fun of(code: ApiResponseCode, success: Boolean) = ApiHeader(code, success)
+        }
+    }
 
     @JsonInclude(JsonInclude.Include.NON_NULL)
-    data class ApiBody<T>(val message: String, val data: T?)
+    data class ApiBody<T> private constructor(
+        private val message: String,
+        private val data: T?
+    ) {
+        companion object {
+            internal fun <T> of(message: String, data: T? = null) = ApiBody(message, data)
+        }
+    }
 
     @JsonInclude(JsonInclude.Include.NON_NULL)
     data class ErrorData private constructor(
-        val timestamp: LocalDateTime,
-        val exception: String,
-        val fieldErrors: List<FieldError>?
+        private val timestamp: LocalDateTime,
+        private val exception: String,
+        private val fieldErrors: List<FieldError>?
     ) {
         companion object {
-            fun of(ex: String, fieldErrors: List<FieldError>? = null) =
+            internal fun of(ex: String, fieldErrors: List<FieldError>? = null) =
                 ErrorData(LocalDateTime.now(), ex, fieldErrors)
         }
 
         data class FieldError private constructor(
-            val field: String,
-            val value: String?,
-            val reason: String?
+            private val field: String,
+            private val value: String?,
+            private val reason: String?
         ) {
             companion object {
-                fun of(result: BindingResult): List<FieldError> =
+                internal fun of(result: BindingResult): List<FieldError> =
                     result.fieldErrors.map { err ->
                         FieldError(err.field, err.rejectedValue?.toString(), err.defaultMessage)
                     }
