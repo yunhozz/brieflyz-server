5b91ed199eb362a3b50ec08031eb0a3855116818
Yunho Park <qkrdbsgh0111@gmail.com>
2025-06-27 18:55:54 +0900

    [#1] STYLE: 검증 실패 메세지 및 조건 추가

--- auth-service/src/main/kotlin/io/brieflyz/auth_service/model/dto/SignInRequestDTO.kt
+++ auth-service/src/main/kotlin/io/brieflyz/auth_service/model/dto/SignInRequestDTO.kt
@@ -1,13 +1,11 @@
 package io.brieflyz.auth_service.model.dto
 
-import jakarta.validation.constraints.Email
 import jakarta.validation.constraints.NotBlank
 
 data class SignInRequestDTO(
-    @field:NotBlank
-    @field:Email
+    @field:NotBlank(message = "이메일을 입력해주세요.")
     val email: String,
 
-    @field:NotBlank
+    @field:NotBlank(message = "비밀번호를 입력해주세요.")
     val password: String
 )
--- auth-service/src/main/kotlin/io/brieflyz/auth_service/model/dto/SignUpRequestDTO.kt
+++ auth-service/src/main/kotlin/io/brieflyz/auth_service/model/dto/SignUpRequestDTO.kt
@@ -2,12 +2,17 @@ package io.brieflyz.auth_service.model.dto
 
 import jakarta.validation.constraints.Email
 import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.Pattern
 
 data class SignUpRequestDTO(
-    @field:NotBlank
-    @field:Email
+    @field:NotBlank(message = "이메일을 입력해주세요.")
+    @field:Email(message = "정확한 형식의 이메일을 입력해주세요.")
     val email: String,
 
-    @field:NotBlank
+    @field:NotBlank(message = "비밀번호를 입력해주세요.")
+    @field:Pattern(
+        regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[!@#\$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>/?]).{8,}\$",
+        message = "비밀번호는 8자 이상이며, 대소문자, 숫자, 특수문자를 포함해야 합니다."
+    )
     val password: String
 )
