ad44e2829d3bd20f57731c96756071f3bce8c6b4
Yunho Park <qkrdbsgh0111@gmail.com>
2025-08-24 23:25:48 +0900

    [#26] CHORE: document-service 프로젝트 추가 및 필요없는 의존성 제거

--- ai-service/build.gradle.kts
+++ ai-service/build.gradle.kts
@@ -11,17 +11,8 @@ dependencies {
     implementation("org.springframework.ai:spring-ai-starter-model-openai")
     // Spring Cloud
     implementation("org.springframework.cloud:spring-cloud-starter-config")
-    // PostgreSQL
-    runtimeOnly("org.postgresql:postgresql")
-    runtimeOnly("org.postgresql:r2dbc-postgresql")
     // Redisson
     implementation("org.redisson:redisson-spring-boot-starter:3.50.0")
-    // Excel POI
-    implementation("org.apache.poi:poi:5.4.1")
-    implementation("org.apache.poi:poi-ooxml:5.4.1")
-    // Powerpoint POI
-    implementation("org.apache.poi:poi-scratchpad:5.4.1")
-    implementation("org.apache.poi:poi-ooxml-full:5.4.1")
     // Reactor
     implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
     // Test
