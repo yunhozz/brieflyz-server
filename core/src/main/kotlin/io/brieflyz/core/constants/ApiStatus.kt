e509ddc74e3d03bad82d996045c29a49bfec56ca
Yunho Park <qkrdbsgh0111@gmail.com>
2025-08-30 14:38:26 +0900

    [#30] REFACTOR: API 상태 리팩토링

--- core/src/main/kotlin/io/brieflyz/core/constants/ApiStatus.kt
+++ core/src/main/kotlin/io/brieflyz/core/constants/ApiStatus.kt
@@ -1,25 +1,26 @@
 package io.brieflyz.core.constants
 
-import io.brieflyz.core.constants.ApiStatusBuilder.badRequest
-import io.brieflyz.core.constants.ApiStatusBuilder.conflict
-import io.brieflyz.core.constants.ApiStatusBuilder.created
-import io.brieflyz.core.constants.ApiStatusBuilder.forbidden
-import io.brieflyz.core.constants.ApiStatusBuilder.internalServerError
-import io.brieflyz.core.constants.ApiStatusBuilder.methodNotAllowed
-import io.brieflyz.core.constants.ApiStatusBuilder.noContent
-import io.brieflyz.core.constants.ApiStatusBuilder.notFound
-import io.brieflyz.core.constants.ApiStatusBuilder.ok
-import io.brieflyz.core.constants.ApiStatusBuilder.serviceUnavailable
-import io.brieflyz.core.constants.ApiStatusBuilder.unauthorized
+data class ApiResult(val statusCode: Int, val message: String)
+
+infix fun Int.ok(message: String) = ApiResult(this, message)
+infix fun Int.created(message: String) = ApiResult(this, message)
+infix fun Int.noContent(message: String) = ApiResult(this, message)
+infix fun Int.badRequest(message: String) = ApiResult(this, message)
+infix fun Int.unauthorized(message: String) = ApiResult(this, message)
+infix fun Int.forbidden(message: String) = ApiResult(this, message)
+infix fun Int.notFound(message: String) = ApiResult(this, message)
+infix fun Int.methodNotAllowed(message: String) = ApiResult(this, message)
+infix fun Int.conflict(message: String) = ApiResult(this, message)
+infix fun Int.internalServerError(message: String) = ApiResult(this, message)
+infix fun Int.serviceUnavailable(message: String) = ApiResult(this, message)
 
 sealed interface ApiStatus {
     val statusCode: Int
-    val message: String
 }
 
 enum class SuccessStatus(
     override val statusCode: Int,
-    override val message: String
+    val message: String
 ) : ApiStatus {
     // 200 OK
     USER_INFORMATION_READ_SUCCESS(200 ok "유저 정보 조회에 성공하였습니다."),
@@ -41,12 +42,12 @@ enum class SuccessStatus(
     SUBSCRIBE_DELETE_SUCCESS(204 noContent "구독이 삭제되었습니다.")
     ;
 
-    constructor(status: ApiStatusBuilder.ApiStatus) : this(status.statusCode, status.message)
+    constructor(result: ApiResult) : this(result.statusCode, result.message)
 }
 
 enum class ErrorStatus(
     override val statusCode: Int,
-    override val message: String
+    val message: String
 ) : ApiStatus {
     // 400 Bad Request
     BAD_REQUEST(400 badRequest "잘못된 요청입니다."),
@@ -79,5 +80,5 @@ enum class ErrorStatus(
     SERVICE_UNAVAILABLE(503 serviceUnavailable "서비스 접속이 원활하지 않습니다. 잠시 후 다시 시도해 주세요.")
     ;
 
-    constructor(status: ApiStatusBuilder.ApiStatus) : this(status.statusCode, status.message)
+    constructor(result: ApiResult) : this(result.statusCode, result.message)
 }
